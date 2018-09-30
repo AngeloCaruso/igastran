09fd7c90e26b54d6559892fc41c0135656443500
AngeloCaruso <angelocaruso.07@gmail.com>
2018-09-29 23:55:42 -0500

    material-kit

--- src/routes.js
+++ src/routes.js
@@ -29,76 +29,24 @@ router.get('/ingresar', (req, res) => {
 
 //Autenticacion
 router.post('/login', (req, res) => {
-    let { usuario_correo, password } = req.body;
-    res.end();
-    //res.redirect('/perfil');
+    
 });
 router.post('/registro', (req, res) => {
     let { nombre, apellidos, usuario, correo, password } = req.body;
     let consulta = 'insert into usuarios(nombre, apellidos, username, password, correo) values (?, ?, ?, ?, ?)';
     mysql.query(consulta, [nombre, apellidos, usuario, correo, password], (err) => {
         if(!err){
-            res.end('registrado');
+            console.log('registrado');
         }else{
             console.log(err);
         }
     });
-    //res.redirect('/perfil');
+    res.redirect('/ingresar');
 });
 
 //logout
 router.get('/logout', (req, res) => {
     res.render('index');
 });
-
-//Perfil
-router.get('/perfil', (req, res) => {
-    res.render('perfil');
-});
-
-//Manejo de usuarios
-router.route('/usuarios')
-    .get((req, res) => {
-        //res.end('Usuario buscado: '+ req.params.id);
-        mysql.query('select * from usuarios where estado = 1', (err, rows) => {
-            if (!err) {
-               res.json(rows);
-            }else{
-                console.log(err);
-            }
-        });
-    })
-    .post((req, res) => {
-        let {nombre, apellidos, username, password, correo} = req.body;
-        let consulta = 'insert into usuarios(nombre, apellidos, username, password, correo) values (?, ?, ?)';
-        mysql.query(consulta, [nombre, apellidos, username, password, correo], (err) => {
-            if (!err) {
-                res.end('Usuario agregado');
-            }else{
-                console.log(err);
-            }
-        });
-    })
-    .put((req, res) => {
-        let {id, nombre, apellidos, username, password, correo} = req.body;
-        let consulta = 'update usuarios set nombre = ?, apellidos = ?, username = ?, correo = ? where id = ?';
-        mysql.query(consulta , [nombre, apellidos, username, password, correo, id], (err) => {
-            if (!err) {
-                res.end('Usuario actualizado');
-            }else{
-                console.log(err);
-            }
-        });
-    })
-    /*.delete((req, res) => {
-        let {id} = req.body;
-        mysql.query('update usuarios set estado = 1 where id = ?', [id], (err) => {
-            if (!err) {
-                res.end('Usuario eliminado');
-            }else{
-                console.log(err);
-            }
-        });
-    })*/;
 
 module.exports = router;
